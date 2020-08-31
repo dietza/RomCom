8ebb25217f4bfe5c122696071777098c0f4beef4
Allison Dietz <dietz.allison@outlook.com>
2020-08-31 00:04:55 -0600

    Initial commit - Create randomized cover object

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 // Create variables targetting the relevant DOM elements here 👇
 
+var coverImg = document.querySelector('.cover-image');
+var coverTitle = document.querySelector('.cover-title');
+var tagline1 = document.querySelector('.tagline-1');
+var tagline2 = document.querySelector('.tagline-2');
+
+
+// change image src
+// document.getElementById('myImageID').src="images/my_other_image.png";
 
 // We've provided a few variables below
 var savedCovers = [
@@ -9,11 +17,45 @@ var currentCover;
 
 // Add your event listeners here 👇
 
+// console.log("savedCovers", savedCovers);
 
 // Create your event handlers and other functions here 👇
+
+window.addEventListener('load', createCover);
+
+
 
 
 // We've provided one function to get you started
 function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
+  var randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
 }
+
+
+
+// use to grab a random cover from the array in data.js file
+// console.log(getRandomIndex(covers));
+// console.log(covers[getRandomIndex(covers)]);
+
+function createCover() {
+
+  var randomCover = getRandomIndex(covers);
+  var randomTitle = getRandomIndex(titles);
+  var randomTag1 = getRandomIndex(descriptors);
+  var randomTag2 = getRandomIndex(descriptors);
+  currentCover = new Cover(randomCover, randomTitle, randomTag1, randomTag2);
+
+}
+
+// the 'covers' array (from data file) is passed in as an argument to the getRandomIndex function.
+// that function returns a randomly generated index number relative to the 'covers' array,
+// which is then
+
+
+///// We need to create an event listener for the page refresh.
+///// When the page is refreshed, the browser will show a new, randomly-selected cover (and title, etc)
+/////
+///// DECLARE the variables for ALL of the DOM elements which will need to be used.
+///// for the '.cover-image' '.cover-title' '.tagline-1' '.tagline-2'
+///// for each (all) of the randomly generated values
